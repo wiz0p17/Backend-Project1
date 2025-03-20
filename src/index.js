baecfa4e4a34d21b91f39b915d676aa466513f5d
Vivek <vivek@Viveks-MacBook-Air.local>
2025-03-20 13:02:26 +0530

    Api error and response handling

--- src/index.js
+++ src/index.js
@@ -10,5 +10,18 @@ dotenv.config({
 
 
 
-connectDB();
+connectDB()
+.then(() => {
+    const port = process.env.PORT || 8000
+    app.listen( port , () => {
+        console.log(`Server is running at PORT : ${port}`);  
+    })
+    app.on('error',(error) => {
+        console.log('ERRR: ',error);  
+        throw error 
+    })
+})
+.catch((err) => {
+    console.log("Mongo DB connection failed",err); 
+})
 
